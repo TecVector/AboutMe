800cab983e25598ed46645263d72b50ed3b5808c
Kevin Cox <Kevin.Cox@byetm.com>
2020-01-28 19:53:19 -0600

    Fixed grammar and spelling mistakes

--- js/app.js
+++ js/app.js
@@ -21,26 +21,26 @@ var questions = [
     ],
     [
         'Are you intersted in someone who values family, and is driven to succeed at work to help their family?',
-        'Great, because I definitely value both family and work.  Having 5 children, I feel it\'s extremely emportant to set an example in all things I do.',
+        'Great, because I definitely value both family and work.  Having 5 children, I feel it\'s extremely important to set an example in all things I do.',
         'Well, then I hope that you\'re willing to give me a chance to work with you and show you the benefits of having someone like me work for you, and hopefully help usher in a new environment for your other staff.'
     ],
     [
         'Would you hire someone who is lazy to do a hard task?  Yes, I realize this is an odd question, but please go with it.',
         'Glad to see you think like Bill Gates!  Although I don\'t consider myself a true lazy person, I do not like to repeat myself, or make more work.  I think it\'s extremely important to spend a little more time upfront to automate and simplify your work, so you can spend more time on improving and creating.',
-        'Well, as Bill Gates once said that he would always "hire a lazy person to do a difficult job" at Microsoft. Why? "Because a lazy person will find an easy way to do it."  Remember, lazy doesn\'t always translate to poor work, but may instead mean proficient work.'
+        'Well, as Bill Gates once said that he would always "hire a lazy person to do a difficult job" at Microsoft. Why? "Because a lazy person will find an easy way to do it."  Remember, lazy doesn\'t always translate to poor work but may instead mean proficient work.'
     ],
     [
         'Let\'s make this a little more personal - do you think I have more cats than kids?',
-        'That would be incorrect.  Being a cat family, we somehow ended up with one for each of my kids - so a total of 5!  Don\'t worry though, no litter box smell here.',
+        'That would be incorrect.  Being a cat family, we somehow ended up with one for each of my kids - so a total of 5!  Don\'t worry though, no litter box smells here.',
         'Great job!  Yes, we have one for each kid.  Definitely wasn\'t a plan, but things fell into play that way.  Gives each of the kids a good chore to do everyday, too!'
     ],
     [
-        'The world of web development if full of different approaches to making websites.  Does having a developer who may not be fluent in your preference yet, but willing to learn it entice you?',
-        'That is great to hear!  I pride myself on my ability to tackle any learning objective put in front of me.  I\'m constantly striving to better my skills, and love the challenges and rewards that come with learning new things.',
+        'The world of web development is full of different approaches to making websites.  Does having a developer who may not be fluent in your preference yet, but willing to learn it entice you?',
+        'That is great to hear!  I pride myself on my ability to tackle any learning objective put in front of me.  I\'m constantly striving to better my skills and love the challenges and rewards that come with learning new things.',
         'Well, with the standards constantly being pushed and new frameworks on the horizon, I\'d be confident in assuming your current staff already has be learn new ways to handle even the current states.  I am always a quick and dedicated student to new and growing stacks, so still feel that you should be willing to take a look.'
     ],
     'Try and guess a number between 1 and 10...',
-    'As mentioned early, I have 5 children - 3 boys, 2 girls.  Can you guess any of their names?'
+    'As mentioned earlier, I have 5 children - 3 boys, 2 girls.  Can you guess any of their names?'
 ]
 var correctAnswers = 0;
 
@@ -49,7 +49,7 @@ for (var x = 0; x < questions.length - 2; x++) {
     var answer = prompt(question);
     var parsedAnswer = isCorrectAnswer(answer);
     while (parsedAnswer === null) {
-        answer = prompt("Please use a Y/N choice for answering this question.  " + questions[x][0]);
+        answer = prompt("Please use a Yes/No or a Y/N choice for answering this question.  " + questions[x][0]);
         parsedAnswer = isCorrectAnswer(answer);
     }
     if (parsedAnswer) {
@@ -60,11 +60,9 @@ for (var x = 0; x < questions.length - 2; x++) {
     }
 }
 
-
-
 var question6 = questions[5];
 var answer6 = Number(prompt(question6));
-var generatedNumber = Math.floor(Math.random() * 10) + 1;
+var generatedNumber = Math.ceil(Math.random() * 10);
 var guessesLeft = 4;
 var isCorrect = false;
 for (var x = guessesLeft; x > 0; x--) {
@@ -136,9 +134,9 @@ if (correctAnswer) {
 }
 
 
-alert('Well, ' + nameOfVisitor + ', I\'m glad you are here!  Please check out my digital resume, and get in touch if you have an questions!');
+alert('Well, ' + nameOfVisitor + ', I\'m glad you\'re here!  Please check out my digital resume, and get in touch if you have any questions!');
 var correctPercent = Math.floor((correctAnswers / questions.length) * 100);
-alert('By the way, you matched my hopeful/expected responsed at ' + correctPercent + '%, getting ' + correctAnswers + '/' + questions.length + ' correct!');
+alert('By the way, you matched my hopeful/expected responses at ' + correctPercent + '%, getting ' + correctAnswers + '/' + questions.length + ' correct!');
 
 
 function isCorrectAnswer(userResponse) {
